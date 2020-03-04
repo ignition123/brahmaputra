4f5fdb083543a2d7389745f920c42479d5a794fa
Sudeep Dasgupta <sudeep.dasgupta@religare.com>
2020-03-04 19:06:31 +0530

    client driver template

--- src/brahmaputra/Brahmaputa.go
+++ src/brahmaputra/Brahmaputa.go
@@ -1,5 +1,28 @@
 package brahmaputra
 
 func Connect(){
+
+// publish code
+	// this.realmProps = FTL.createProperties();
+ //            this.realmProps.set(Realm.PROPERTY_STRING_USERNAME, TCP_CONFIG.FTL_USER_NAME);
+ //            this.realmProps.set(Realm.PROPERTY_STRING_USERPASSWORD, TCP_CONFIG.FTL_PASSWORD);
+ //            this.realmProps.set(Realm.PROPERTY_STRING_CLIENT_LABEL, TCP_CONFIG.FTL_LABEL);
+
+ //            this.realm = FTL.connectToRealmServer(TCP_CONFIG.FTL_URL,TCP_CONFIG.PUB_APP_NAME, this.realmProps);
+ //            this.pub = this.realm.createPublisher(TCP_CONFIG.PUB_Endpoint_Name);
+
+ //            this.msg = this.realm.createMessage(null);	
+
+
+// subscribe code
+
+ // this.props = FTL.createProperties();
+ //            this.props.set(Realm.PROPERTY_STRING_USERNAME, TCP_CONFIG.FTL_USER_NAME);
+ //            this.props.set(Realm.PROPERTY_STRING_USERPASSWORD, TCP_CONFIG.FTL_PASSWORD);
+ //            this.realm = FTL.connectToRealmServer(TCP_CONFIG.FTL_URL, TCP_CONFIG.SUB_APP_NAME , this.props);
+ //            this.cm = this.realm.createContentMatcher("{\"Exchange\":\"NSE\",\"ExchangeSegment\":\"CM\"}");
+ //            this.sub = this.realm.createSubscriber(TCP_CONFIG.SUB_Endpoint_Name, this.cm, null);
+ //            this.queue = this.realm.createEventQueue();
+ //            this.queue.addSubscriber(this.sub, this);
 	
 }
